ca2b4999cf8dbe3d8c0e48da0023302d3c35f4f5
Raphael Randschau <nicolai86@me.com>
2012-05-15 20:42:26 +0200

    remove the schema

--- lib/devise_cas_authenticatable/schema.rb
+++ lib/devise_cas_authenticatable/schema.rb
@@ -1,15 +1,16 @@
-require 'devise/schema'
-
-module Devise
-  module Schema
-    # Adds the required fields for cas_authenticatable to the schema.  Currently
-    # this is just username (String).
-    def cas_authenticatable
-      if respond_to? :apply_devise_schema
-        apply_devise_schema :username, String
-      else
-        apply_schema :username, String
-      end
-    end
-  end
-end
+# require 'devise/schema'
+# 
+# module Devise
+#   module Schema
+#     # Adds the required fields for cas_authenticatable to the schema.  Currently
+#     # this is just username (String).
+#     def cas_authenticatable
+#       if respond_to? :apply_devise_schema
+#         apply_devise_schema :username, String
+#       else
+#         apply_schema :username, String
+#       end
+#     end
+#   end
+# end
+# 
